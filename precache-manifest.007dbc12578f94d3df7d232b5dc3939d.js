cff47bd278722269f1995eeb093a34f327124475
Tanush Goel <tanushgoel03@gmail.com>
2022-02-14 16:50:13 -0800

    Updates

---
+++ precache-manifest.007dbc12578f94d3df7d232b5dc3939d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a1914165efdd79e1a2bbdd712b4c9a6",
+    "url": "//tanushgo.github.io//favorite-classes/index.html"
+  },
+  {
+    "revision": "4cca8fb72632d39fefee",
+    "url": "//tanushgo.github.io//favorite-classes/static/css/main.af65d1e4.chunk.css"
+  },
+  {
+    "revision": "8b8d5a007a02500c6c63",
+    "url": "//tanushgo.github.io//favorite-classes/static/js/2.624f555d.chunk.js"
+  },
+  {
+    "revision": "68c47fc33538f58652f4",
+    "url": "//tanushgo.github.io//favorite-classes/static/js/3.987c3a98.chunk.js"
+  },
+  {
+    "revision": "4cca8fb72632d39fefee",
+    "url": "//tanushgo.github.io//favorite-classes/static/js/main.c330c679.chunk.js"
+  },
+  {
+    "revision": "339f7d10aeefb1a6d195",
+    "url": "//tanushgo.github.io//favorite-classes/static/js/runtime~main.82306862.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "//tanushgo.github.io//favorite-classes/static/media/logo.06e73328.svg"
+  }
+]);
