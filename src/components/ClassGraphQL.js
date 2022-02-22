560575d9b5f2838d5bf8e11c1370b90639d83fc2
Tanush Goel <tanushgoel03@gmail.com>
2022-02-21 16:08:15 -0800

    ADDING PROFESSORS

--- src/components/ClassGraphQL.js
+++ src/components/ClassGraphQL.js
@@ -6,6 +6,7 @@ function ClassGraphQL(props) {
 
     const [classInfo, setClassInfo] = useState({});
 
+
     const url = "https://api.peterportal.org/graphql"
 
 
@@ -17,10 +18,13 @@ function ClassGraphQL(props) {
                         title
                         department_name
                         description
+                        instructor_history{
+                            name
+                        }
                     }
                 }
             `
-
+            
             const response = await fetch(url, {
                 method: "POST",
                 body: JSON.stringify({query}),
@@ -31,17 +35,26 @@ function ClassGraphQL(props) {
             const data = await response.json();
             console.log(data);
             setClassInfo(data.data.course);
-
+        
         }
         fetchData();
     }, [props.name]);
     let info;
-    if (classInfo) {
+    let instructors='';
+    if ((classInfo) && classInfo.instructor_history) {
+        console.log(classInfo.instructor_history)
+        for (let i = 0; i < classInfo.instructor_history.length; i++) {
+            if (classInfo.instructor_history[i]){
+                instructors +=classInfo.instructor_history[i].name +' , ';
+            }
+          }
+        
         info = <div className="information">
             <p id="title"> {classInfo.title} </p>
             <p id="department">{classInfo.department_name}</p>
             <p id="description">{classInfo.description}</p>
-        </div>
+            <p id="instructor">Professors : {instructors.slice(0,-2)}</p>
+            </div>
     } else if (classInfo == null) {
         info = <p>Class Not Found</p>
     } else {
